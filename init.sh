28e54a058613c563d15caf1a732afb0c1c6d1d02
tempolo <62590209+tempolo@users.noreply.github.com>
2020-08-24 00:46:37 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -72,9 +72,7 @@ curl -OL https://github.com/jpillora/chisel/releases/download/v1.6.0/chisel_1.6.
 gzip -d chisel_1.6.0_linux_amd64.gz
 chmod +x chisel_1.6.0_linux_am*
 mv chisel_1.6.0_linux_amd64 chisel
-ls
-./chisel -p $PORT --proxy http://127.0.0.1:3333
-./chisel --port $PORT --proxy http://127.0.0.1:3333 --auth <root:root>
+./chisel server --port $PORT --proxy http://127.0.0.1:3333 --auth <root:root>
 
 
 
